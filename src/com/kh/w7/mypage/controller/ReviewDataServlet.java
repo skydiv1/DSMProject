63ec05eace8d7bbaa309fe56d369fcfec618f7c3
di20819 <di20819@naver.com>
2019-01-08 18:03:17 +0900

    페이징 전 기능 다끝나따아아아아아아아으ㅏㅇ아아ㅏ이ㅏ

---
+++ src/com/kh/w7/mypage/controller/ReviewDataServlet.java
@@ -0,0 +1,52 @@
+package com.kh.w7.mypage.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+import com.kh.w7.member.model.vo.Member;
+import com.kh.w7.mypage.model.service.MypageService;
+
+@WebServlet("/reviewstar.seller")
+public class ReviewDataServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ReviewDataServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		
+		int loginCode = loginUser.getMemberCode();
+		int rstar = Integer.parseInt(request.getParameter("rstar"));
+		int productNo = Integer.parseInt(request.getParameter("productNo"));
+		String rtext = request.getParameter("rtext");
+		
+		System.out.println("loginCode 리뷰:"+loginCode);
+		
+		int result = new MypageService().ReviewUpdate(loginCode,rstar,rtext,productNo);
+		System.out.println("loginCode 리뷰:"+loginCode);
+		
+		System.out.println("result 리뷰:"+result);
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		new Gson().toJson(result, response.getWriter());
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/kh/w7/mypage/controller/ReviewStateServlet.java
@@ -0,0 +1,39 @@
+package com.kh.w7.mypage.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.kh.w7.mypage.model.service.MypageService;
+
+@WebServlet("/reviewstate.seller")
+public class ReviewStateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ReviewStateServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int dealnum = Integer.parseInt(request.getParameter("dealnum"));
+	
+		int result = new MypageService().reviewStateUpdate(dealnum);
+		
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		new Gson().toJson(result, response.getWriter());
+	
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
